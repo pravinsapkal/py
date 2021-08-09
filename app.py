8f6e91f7a74b6a2c60e4a6da0991d956be56e028
apple <apple@Pravins-MacBook-Pro.local>
2021-08-09 12:04:29 +0530

    predict api response

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, jsonify
 
 import marks
 
 app = Flask(__name__)
+
+@app.route("/predict", methods = ['POST'])
+def predict():
+    pred = mks_pdc = 0
+    if request.method == "POST":
+        hrs = request.form["hrs"]
+        if hrs:
+            mks_pdc = marks.marks_prediction(hrs)
+
+    data = {'prediction': mks_pdc[0][0], 'hrs': hrs}
+    return jsonify(data)
 
 @app.route("/", methods = ['GET', 'POST'])
 def hello():
@@ -15,5 +27,8 @@ def hello():
 
     return render_template("index.html", pred = mks_pdc)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
